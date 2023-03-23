53ac13191f209f6c7a8da20c2e73336a6c4a7267
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-03-23 11:04:49 +0100

    apartment_seeder_done

---
+++ config/apartment_db.php
@@ -0,0 +1,103 @@
+<?php
+
+return [
+    [
+        'id' => '1',
+        'title' => 'Villa Cefalù',
+        'n_room' => 2,
+        'n_bed' => 3,
+        'n_bathroom' => 2,
+        'mq' => '190mq',
+        'image' => 'https://a0.muscache.com/im/pictures/miso/Hosting-49699441/original/198f25d3-2cd7-4077-bde6-aa322f2bea16.jpeg?im_w=960',
+        'latitude' => '38.03074',
+        'longitude' => '14.03700'
+    ],
+    [
+        'id' => '2',
+        'title' => 'Suite privata con trullo Martodda',
+        'n_room' => 1,
+        'n_bed' => 1,
+        'n_bathroom' => 2,
+        'mq' => '300mq',
+        'image' => 'https://a0.muscache.com/im/pictures/db99f8a7-bd26-4383-a415-0818dd39e301.jpg?im_w=960',
+        'latitude' => '42.73779',
+        'longitude' => '12.31786'
+    ],
+    [
+        'id' => '3',
+        'title' => 'Villa Darda',
+        'n_room' => 2,
+        'n_bed' => 5,
+        'n_bathroom' => 1,
+        'mq' => '100mq',
+        'image' => 'https://a0.muscache.com/im/pictures/ff280417-8d90-487b-9498-c9df4ad6d30a.jpg?im_w=960',
+        'latitude' => '38.02205',
+        'longitude' => '12.91031'
+    ],
+    [
+        'id' => '4',
+        'title' => 'Baita Vrei',
+        'n_room' => 2,
+        'n_bed' => 3,
+        'n_bathroom' => 1,
+        'mq' => '392mq',
+        'image' => 'https://a0.muscache.com/im/pictures/c655192a-7599-4e77-a2ee-a42f4365d7b4.jpg?im_w=960',
+        'latitude' => '45.98057',
+        'longitude' => '8.50118'
+    ],
+    [
+        'id' => '5',
+        'title' => 'La Torre Dell\'Oliveto',
+        'n_room' => 8,
+        'n_bed' => 16,
+        'n_bathroom' => 4,
+        'mq' => '856mq',
+        'image' => 'https://a0.muscache.com/im/pictures/1bfbd9f0-9703-4334-a0e8-a1a4e19846e6.jpg?im_w=960',
+        'latitude' => '43.06903',
+        'longitude' => '12.25559'
+    ],
+    [
+        'id' => '6',
+        'title' => 'Villa Di Maria',
+        'n_room' => 3,
+        'n_bed' => 9,
+        'n_bathroom' => 2,
+        'mq' => '452mq',
+        'image' => 'https://a0.muscache.com/im/pictures/ed210411-8c4e-481b-a4ff-13e27a1f90e3.jpg?im_w=960',
+        'latitude' => '38.17685',
+        'longitude' => '12.73337'
+    ],
+    [
+        'id' => '7',
+        'title' => 'Trullo magico ad Ostuni',
+        'n_room' => 2,
+        'n_bed' => 5,
+        'n_bathroom' => 1,
+        'mq' => '250mq',
+        'image' => 'https://a0.muscache.com/im/pictures/30d6cf52-d303-4026-8d8c-5cc8ac03444e.jpg?im_w=960',
+        'latitude' => '40.70796',
+        'longitude' => '17.47579'
+    ],
+    [
+        'id' => '8',
+        'title' => 'Pigna Felice Boutique Superior',
+        'n_room' => 1,
+        'n_bed' => 2,
+        'n_bathroom' => 0,
+        'mq' => '20mq',
+        'image' => 'https://a0.muscache.com/im/pictures/f66858d4-ad44-4d04-831a-289080742796.jpg?im_w=960',
+        'latitude' => '42.84318',
+        'longitude' => '10.77794'
+    ],
+    [
+        'id' => '9',
+        'title' => 'Alloggio incantevole con piscina',
+        'n_room' => 4,
+        'n_bed' => 8,
+        'n_bathroom' => 3,
+        'mq' => '123mq',
+        'image' => 'https://a0.muscache.com/im/pictures/miso/Hosting-617236405053732513/original/56e940f3-c0a9-43e9-8c62-d903c92190ad.jpeg?im_w=960',
+        'latitude' => '37.60868',
+        'longitude' => '15.09523'
+    ]
+];
