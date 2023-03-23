16c7107e8d914d061bcccfa74effc759333c8444
ilario-moreni <ilario2001.mor@gmail.com>
2023-03-23 12:20:46 +0100

    fixed seeder

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Apartment;
+use Illuminate\Support\Str;
 
 class ApartmentSeeder extends Seeder
 {
@@ -16,11 +17,13 @@ class ApartmentSeeder extends Seeder
     public function run()
     {
         $apartments = config('apartment_db');
+        
 
-        foreach ($apartments as $apartment) {
+        foreach ($apartments as $key=>$apartment) {
             $newApt = new Apartment();
             $newApt->id = $apartment['id'];
             $newApt->title = $apartment['title'];
+            $newApt->slug = Str::slug($newApt->title, '-');
             $newApt->n_room = $apartment['n_room'];
             $newApt->n_bed = $apartment['n_bed'];
             $newApt->n_bathroom = $apartment['n_bathroom'];
@@ -28,6 +31,9 @@ class ApartmentSeeder extends Seeder
             $newApt->image = $apartment['image'];
             $newApt->latitude = $apartment['latitude'];
             $newApt->longitude = $apartment['longitude'];
+
+            $newApt->save();
         }
+        
     }
 }
