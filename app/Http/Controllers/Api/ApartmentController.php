4045126a9059d5dccabcff9e7d83f27f9bfaa58e
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-03-28 17:35:10 +0200

    apartment_controller_api_completed

---
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Apartment;
+
+class ApartmentController extends Controller
+{
+    public function index(Request $request){
+        $apartments = Apartment::with('services', 'sponsorships')->paginate(8);
+        return response()->json([
+            'success' => true,
+            'apartments' => $apartments
+        ]);
+    }
+
+    public function show($slug){
+        $apartment = Apartment::with('services', 'sponsorships')->where('slug', $slug)->first();
+
+        if($apartment){
+            return response()->json([
+                'success' => true,
+                'apartment' => $apartment,
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun appartamento trovato',
+            ]);
+        }
+    }
+}
