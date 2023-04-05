dd867fd915b281fa8d73b426705b5ba16d467c00
Mark-dev30 <fronterremarco@gmail.com>
2023-04-05 18:51:22 +0200

    fixed range and order by distance

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -38,51 +38,7 @@ class ApartmentController extends Controller
      */
     public function create()
     {
-        //TEST
-        $lat1 = 41.90232;
-        $lon1 = 12.50028;
-        $street = 'via%20aldo%20carratore';
 
-        $idarray = [];
-        $resultApartments = $this->getRadiusCenter($street, 20);
-        foreach ($resultApartments as $apartment) {
-            array_push($idarray, $apartment->id);
-        }
-        /* $idapartment = Apartment::whereIn('id', $idarray)->where('n_room', '>=', 1)->where('n_bed', '>=', 1)->where('n_bathroom', '>=', 1)->when($lon1 and $lat1, function ($query) use ($lat1, $lat2, $lon1, $lon2) {
-            $query->addSelect(DB::raw("ST_Distance_Sphere(POINT('$lon1', '$lat1'), POINT('$lon2', '$lat2')) as distance"))->orderBy('distance');
-        })->get(); */
-        $cordinates = $this->getCordinates($street);
-
-        $idapartment = Apartment::whereIn('id', $idarray)->where('n_room', '>=', 1)->where('n_bed', '>=', 1)->where('n_bathroom', '>=', 1)->select(['*'])->selectRaw("(6371 * acos(cos(radians($cordinates[0])) * cos(radians(latitude)) * cos(radians(longitude) - radians($cordinates[1])) + sin(radians($cordinates[0])) * sin(radians(latitude)))) AS distance")->havingRaw("distance < 10")->orderBy('distance')->get();
-        dd($idapartment);
-
-
-
-
-        //Prova 1
-        $results =  Apartment::orderByDistanceFrom(41.90232, 12.50028, 'asc')->get();
-
-        //Prova 2
-        $lat1 = 41.90232;
-        $lon1 = 12.50028;
-        $lat2 = 41.89766;
-        $lon2 = 12.42066;
-        $dist = 20;
-        $prova2 = Apartment::select(['id', 'title'])->when($lon1 and $lat1, function ($query) use ($lat1, $lat2, $lon1, $lon2) {
-            $query->addSelect(DB::raw("ST_Distance_Sphere(POINT('$lon1', '$lat1'), POINT('$lon2', '$lat2')) as distance"))->orderBy('distance');
-        })->get();
-        dd($prova2);
-        /* $prova3 = Apartment::select(['*'])->selectRaw("(6371 * acos(cos(radians($lat1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($lon1)) + sin(radians($lat1)) * sin(radians(latitude)))) AS distance")->orderBy('latitude')->orderBy('longitude')->havingRaw("distance < $dist")->get();
-        dd($prova3); */
-
-
-
-
-
-
-
-
-        //FINE TEST
         /* indirizzamento alla pagina di creazione di un nuovo apartment */
         $services = Service::all();
         return view('admin.apartments.create', compact('services'));
@@ -230,58 +186,5 @@ class ApartmentController extends Controller
         $apartment->delete();
         //Reindirizza alla pagina index.
         return redirect()->route('admin.apartments.index')->with('message', 'Annuncio cancellato correttamente');
-    }
-    public function getRadiusCenter($a, $b)
-    {
-        $cordinates = $this->getCordinates($a);
-
-        $apartments = Apartment::all();
-        $AptString = '';
-        foreach ($apartments as $key => $apartment) {
-            if ($key === (count($apartments) - 1)) {
-                $AptPosition = "%7B%22position%22%3A%7B%22lat%22%3A" . $apartment->latitude . "%2C%22lon%22%3A" . $apartment->longitude . "%7D%7D";
-                $AptString = $AptString . $AptPosition;
-            } else {
-                $AptPosition = "%7B%22position%22%3A%7B%22lat%22%3A" . $apartment->latitude . "%2C%22lon%22%3A" . $apartment->longitude . "%7D%7D%2C";
-
-                $AptString = $AptString . $AptPosition;
-            }
-        }
-        $r_ange = '';
-        if ($b != null) {
-            $r_ange = $b * 1000;
-        } else {
-            $r_ange = 20000;
-        }
-        $apikey = 'sqAC6HGqUo0FuWA7iea7gmbV4KpA2wju';
-        $radiusUrl = "https://api.tomtom.com/search/2/geometryFilter.json?geometryList=%5B%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22" . $cordinates[0] . "%2C" . $cordinates[1] . "%22%2C%20%22radius%22%3A" . $r_ange . "%7D%2C%20%7B%22type%22%3A%22POLYGON%22%2C%20%22vertices%22%3A%5B%2237.7524152343544%2C%20-122.43576049804686%22%2C%20%2237.70660472542312%2C%20-122.43301391601562%22%2C%20%2237.712059855877314%2C%20-122.36434936523438%22%2C%20%2237.75350561243041%2C%20-122.37396240234374%22%5D%7D%5D&poiList=%5B" . $AptString . "%5D&key=" . $apikey;
-        $filter = Http::withOptions(['verify' => false])->get($radiusUrl);
-        $positionFilter = $filter->json();
-        $apartmentFilter = [];
-        foreach ($positionFilter['results'] as  $position) {
-
-            $pos_lon = $position['position']['lon'];
-            $pos_lat = $position['position']['lat'];
-            $singleFilter = Apartment::where('longitude', $pos_lon)->where('latitude', $pos_lat)->first();
-
-            array_push($apartmentFilter, $singleFilter);
-        }
-        return $apartmentFilter;
-    }
-    public function getCordinates($a)
-    {
-        $apikey = 'sqAC6HGqUo0FuWA7iea7gmbV4KpA2wju';
-        $searchUrlFirst = 'https://api.tomtom.com/search/2/geocode/';
-        $searchUrlSecond = '.json?storeResult=false&countrySet=IT&language=it-IT&view=Unified&key=';
-        $newUrl = $searchUrlFirst . $a . $searchUrlSecond . $apikey;
-        $res = Http::withOptions(['verify' => false])->get($newUrl);
-        $response = $res->json();
-        $lat = $response['results'][0]['position']['lat'];
-        $lon = $response['results'][0]['position']['lon'];
-        $cordinates = [];
-        array_push($cordinates, $lat);
-        array_push($cordinates, $lon);
-
-        return $cordinates;
     }
 }
