401228202b9d9c90dd853239eb25392289eaf309
ilario-moreni <ilario2001.mor@gmail.com>
2023-04-07 17:49:20 +0200

    carmellaaa

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -2,30 +2,29 @@
 @section('content')
     <div>
         <div class="container">
-            <div class="row">
-                <div class="col">
-                    {{ $messages_count }} nuovi messaggi.
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            @foreach ($messages as $message)
-                <div class="row ">
-                    <div class="col-8 my-3">
-                        <div class="card border border-primary border-3">
+            <table class="table bg-light-transparent mt-5" style='border-bottom-width:0;'>
+                <thead>
+                    <tr>
+                        <th scope="col" class="width-40-table">Mittente</th>
+                        <th scope="col">Contenuto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (array_reverse($messages) as $message)
+                        <tr class='tr_top'>
+                            <td class='mail_color'>{{ $message->name }} {{ $message->surname }}</td>
+                            <td rowspan="2">
+                                {{ $message->description }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class='mail_color'>{{ $message->email }}</td>
+                        </tr>
+                    @endforeach
 
-                            <h5 class="card-title p-2 border-bottom border-primary">{{ $message->name }}
-                                {{ $message->surname }}</h5>
-                            <div class="d-flex justify-content-between px-2 border-bottom border-primary">
-                                <h6 class="card-subtitle  text-muted">{{ $message->email }}</h6>
-                                <p class="fs-6 card-subtitle  text-muted">{{ $message->created_at }}</p>
-                            </div>
-                            <p class="card-text p-2 mt-2">{{ $message->description }}</p>
-
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
+    <script src="{{ asset('js/showMessage.js') }}"></script>
 @endsection
