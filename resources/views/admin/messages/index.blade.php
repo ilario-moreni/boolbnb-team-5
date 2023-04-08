c2832df4adcd35c0b368269b4dd1dff7c216170a
ilario-moreni <ilario2001.mor@gmail.com>
2023-04-08 15:19:14 +0200

    aye

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <table class="table bg-light-transparent mt-5" style='border-bottom-width:0;'>
                 <thead>
-                    <tr>
+                    <tr class='tr_top' style='background-color:black; color: white;'>
                         <th scope="col" class="width-40-table">Mittente</th>
                         <th scope="col">Contenuto</th>
                     </tr>
@@ -12,13 +12,14 @@
                 <tbody>
                     @foreach (array_reverse($messages) as $message)
                         <tr class='tr_top'>
-                            <td class='mail_color'>{{ $message->name }} {{ $message->surname }}</td>
+                            <td class='mail_color'><strong>Nome:</strong><br> {{ $message->name }} {{ $message->surname }}
+                            </td>
                             <td rowspan="2">
                                 {{ $message->description }}
                             </td>
                         </tr>
-                        <tr>
-                            <td class='mail_color'>{{ $message->email }}</td>
+                        <tr class="tr_lato">
+                            <td class='mail_color'><strong>Mail:</strong><br> {{ $message->email }}</td>
                         </tr>
                     @endforeach
 
